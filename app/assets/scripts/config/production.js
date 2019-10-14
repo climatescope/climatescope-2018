3e43b6da5cfcad8aa50e3e1b49ace42113c5a3e7
Olaf Veerman <olaf.veerman@gmail.com>
2019-10-14 12:19:34 -0400

    Clean up for archival No need to keep CI around. Change the URL of the edition

--- app/assets/scripts/config/production.js
+++ app/assets/scripts/config/production.js
@@ -8,5 +8,5 @@ export default {
   mediumLatestUrl: 'https://cs-medium-latest.herokuapp.com',
   mailchimpUrl: 'https://global-climatescope.us14.list-manage.com/subscribe/post?u=f7420b38cc86494fc1ab6cb31&id=e17a5880bd',
   gaTrackingID: 'UA-56170738-1',
-  mbtoken: null
+  mbtoken: 'pk.eyJ1IjoiY2xpbWF0ZXNjb3BlIiwiYSI6ImNpdzJmb2dwcjBhMzQyenBia2E1azBjODUifQ.9I6shKgqM1xeBA13VX5a4g'
 }
